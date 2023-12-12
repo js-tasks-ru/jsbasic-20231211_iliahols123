766e18fee17a802077f18c4a4d5277a1db272dfd
halch21 <iliahols123@gmail.com>
2023-12-12 23:23:44 +0300

    отредактировал решение первой задачи

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,10 +1,10 @@
-a = 1;
-b = 1;
-let sum_1 = a+b;
-console.log (sum_1);
-c = 2; 
-d = 5; 
-let sum_2 = c + d; 
-console.log (sum_2);
+let a = 1;
+let b = 1;
+let sum_1 = a + b;
+console.log(sum_1);
+let c = 2;
+let d = 5;
+let sum_2 = c + d;
+console.log(sum_2);
 
 
